1ab5a09627fa7e40bc4e054b3361475332d9c85e
Vledder <remi@studiospindle.nl>
2020-03-05 21:01:09 +0100

    cli + server: make endpoint for searching a specific camera by number (can also return multiple cameras)

--- cli/src/search.js
+++ cli/src/search.js
@@ -7,9 +7,14 @@ const map = require("through2-map");
 const apiUrl = 'http://localhost:5000/api/v1/cameras';
 
 const argv = yargs
-  .command('name', 'Search a camera by name', {
+  .command('name', 'Search cameras by name', {
     alias: 'n',
     description: 'the name to search for',
+    type: 'string'
+  })
+  .command('id', 'Search cameras by id', {
+    alias: 'n',
+    description: 'the id to search for',
     type: 'string'
   })
   .argv;
@@ -19,24 +24,39 @@ if (argv.name) {
     .then(checkFetchStatus)
     .then((res) => {
       res.body
-        .pipe(map(findMatch))
-        .pipe(map(formatOutput))
+        .pipe(map(findMatchByName))
+        .pipe(map(formatJSONToBuffer))
         .pipe(process.stdout)
     });
 }
 
-function findMatch(chunk) {
-  const normalizedChunk = chunk.toString().toLowerCase();
-  const normalizedName = argv.name.toLowerCase();
-  if (normalizedChunk.includes(normalizedName)) {
-    return chunk;
-  }
+if (argv.id) {
+  fetch(`${apiUrl}/${argv.id}`)
+    .then((res) => res.json())
+    .then((json) => {
+      const formattedCameras = json.map((camera) => formatObjectToOutputString(camera))
+      process.stdout.write(Buffer.from(formattedCameras.join('').toString()))
+    })
 }
 
-function formatOutput(chunk) {
+function formatJSONToBuffer(chunk) {
   const { Number, Camera, Latitude, Longitude } = JSON.parse(chunk.toString());
   const formattedOutput = `${Number} | ${Camera} | ${Latitude} | ${Longitude} \n`
   return Buffer.from(formattedOutput);
+}
+
+function formatObjectToOutputString(obj) {
+  const { Number, Camera, Latitude, Longitude } = obj;
+  const formattedOutput = `${Number} | ${Camera} | ${Latitude} | ${Longitude} \n`
+  return formattedOutput;
+}
+
+function findMatchByName(chunk) {
+  const normalizedChunk = chunk.toString().toLowerCase();
+  const normalizedName = argv.name.toString().toLowerCase();
+  if (normalizedChunk.includes(normalizedName)) {
+    return chunk;
+  }
 }
 
 function checkFetchStatus(res) {
--- server/routes/cameras.js
+++ server/routes/cameras.js
@@ -3,17 +3,52 @@ const express = require("express");
 const cameras = express.Router();
 const map = require("through2-map");
 const csv2json = require("csv2json"); // 18.4 kB
-const firstThreeDigits = new RegExp(/[\s]*\d{3}/);
+const firstThreeDigits = new RegExp(/[\s]*\d{3}|$/);
 
 cameras.get("/", function(_, res){
   res.contentType("application/json");
   fs.createReadStream('./data/cameras-defb.csv')
+    .pipe(map(removeErrors))
     .pipe(map(addCameraNumber))
     .pipe(csv2json({
       separator: ';'
     }))
     .pipe(res);
 });
+
+cameras.get("/:camera_id", function(req, res) {
+  res.contentType("application/json");
+  fs.createReadStream('./data/cameras-defb.csv')
+    .pipe(map(removeErrors))
+    .pipe(map(addCameraNumber))
+    .pipe(map((chunk) => {
+      const jsonKeys = chunk.toString().split('\n')[0].split(';');
+      const rawCameraValues = filterByNumber(chunk, req.params.camera_id).toString().split(',');
+      const cameras = rawCameraValues.map((rawValues) => {
+        const rawValueArray = rawValues.split(';');
+        let cameraObject = {};
+        jsonKeys.map((key, index) => {
+          return cameraObject[key] = rawValueArray[index];
+        });
+        return cameraObject;
+      });
+      res.send(cameras)
+    }));
+});
+
+function filterByNumber(chunk, id) {
+  const chunkArr = chunk.toString().split('\n');
+  return chunkArr.filter((item) => firstThreeDigits.exec(item.toString())[0] === id);
+}
+
+function removeErrors(chunk) {
+  const chunkArr = chunk.toString().split('\n');
+  const filteredItems = chunkArr.filter((item) => {
+    const error = new RegExp(/^(ERROR)/)
+    return !error.test(item.toString())
+  })
+  return filteredItems.toString().split(',').join("\n");
+}
 
 function addCameraNumber(chunk) {
   const chunkArr = chunk.toString().split('\n');
